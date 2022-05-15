cb010507473b2be7597c494115fccba616798f17
rawkush <ankushrawat950@gmail.com>
2022-05-15 15:36:40 +0530

    added first user event test

--- src/pages/SignUpPage.js
+++ src/pages/SignUpPage.js
@@ -2,14 +2,43 @@ import React from 'react';
 
 class SignUpPage extends React.Component {
 
+  state = {
+    disabled: true,
+    password: '',
+    repeatPassword: ''
+  }
+  onchangePassword = (event) => {
+    const curentValue = event.target.value;
+    this.setState({
+      password: curentValue,
+      disabled: curentValue !== this.state.repeatPassword
+    })
+  }
+  onchangeRepeatPassword = (event) => {
+    const curentValue = event.target.value;
+    this.setState({
+      repeatPassword: curentValue,
+      disabled: curentValue !== this.state.password
+    })
+  }
   render() {
+
     return (
       <div>
         <h1>Sign Up</h1>
         <label htmlFor={"username"} >Username</label>
         <input id="username" placeholder="username" />
+
         <label htmlFor={"email"}>E-mail</label>
         <input id="email" placeholder="email" />
+
+        <label htmlFor={"password"}>password</label>
+        <input id="password" type="password" onChange={this.onchangePassword} placeholder="password" />
+
+        <label htmlFor={"repeat-password"}>repeat password</label>
+        <input id="repeat-password" onChange={this.onchangeRepeatPassword} type="password" placeholder="password" />
+
+        <button disabled={this.state.disabled}>Sign up</button>
       </div >
     )
   }
--- src/pages/SignUpPge.test.js
+++ src/pages/SignUpPge.test.js
@@ -1,6 +1,6 @@
 import SignUpPage from "./SignUpPage"
 import { render, screen } from "@testing-library/react"
-
+import userEvents from "@testing-library/user-event"
 describe('Sign Up Page', () => {
 
   describe('Layout', () => {
@@ -37,6 +37,54 @@ describe('Sign Up Page', () => {
       // const inputs = container.querySelectorAll("input");
       const input = screen.getByLabelText("E-mail");
       expect(input).toBeInTheDocument();
+    })
+
+    it('has password input', () => {
+      const { container } = render(<SignUpPage />);
+      // const inputs = container.querySelectorAll("input");
+      const input = screen.getByLabelText("password");
+      expect(input).toBeInTheDocument();
+    })
+    it('has password type for password input', () => {
+      render(<SignUpPage />);
+      // const inputs = container.querySelectorAll("input");
+      const input = screen.getByLabelText("password");
+      expect(input.type).toBe("password");
+    })
+    it('has password type for password reapeat input', () => {
+      render(<SignUpPage />);
+      // const inputs = container.querySelectorAll("input");
+      const input = screen.getByLabelText("repeat password");
+      expect(input.type).toBe("password");
+    })
+    it('has Sign Up button', () => {
+      render(<SignUpPage />);
+      const btn = screen.queryByRole('button', {
+        name: "Sign up"
+      });
+      expect(btn).toBeInTheDocument();
+    })
+    it('has disables the button initally', () => {
+      render(<SignUpPage />);
+      const btn = screen.queryByRole('button', {
+        name: "Sign up"
+      });
+      expect(btn).toBeDisabled();
+    });
+  })
+
+  describe('interactions', () => {
+    it("enables the button when password amd password-repat fields have some value", () => {
+      render(<SignUpPage />);
+      const repeatPasswordInput = screen.getByLabelText("repeat password");
+      const passwordInput = screen.getByLabelText("password");
+      //typiing data inpto password fields
+      userEvents.type(passwordInput, "password")
+      userEvents.type(repeatPasswordInput, "password");
+      const button = screen.getByRole('button', {
+        name: "Sign up"
+      })
+      expect(button).toBeEnabled();
     })
   })
 
